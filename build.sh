8bfe9dfaf9d5b34cbee8c9933c6c49eccac1d1ee
JAISHNAVPRASAD <rbinternationalnetwork@gmail.com>
2021-04-07 12:43:59 +0000

    SCRIPT: Import script
    Co-Authored-by: Ayush <ayush@disroot.org>
    Signed-off-by: JAISHNAVPRASAD <rbinternationalnetwork@gmail.com>

--- build.sh
+++ build.sh
@@ -1,161 +1,92 @@
-#!/usr/bin/env bash
-# Copyright (C) 2018 Abubakar Yagob (blacksuan19)
-# Copyright (C) 2018 Rama Bondan Prakoso (rama982)
-# SPDX-License-Identifier: GPL-3.0-or-later
+#!/bin/bash
 
-# Color
-green='\033[0;32m'
-echo -e "$yellow"
+BOLD='\033[1m'
+GRN='\033[01;32m'
+CYAN='\033[0;36m'
+YELLOW='\033[1;33m'
+RED='\033[01;31m'
+RST='\033[0m'
+echo "Cloning dependencies if they don't exist...."
 
-# Main Environment
-KERNEL_DIR=$PWD
-KERN_IMG=$KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb
-ZIP_DIR=$KERNEL_DIR/AnyKernel3
-CONFIG=oppo6771_18611_defconfig
-CORES=$(grep -c ^processor /proc/cpuinfo)
-THREAD="-j$CORES"
-CROSS_COMPILE+="$PWD/aarch64-linux-android-4.9/bin/aarch64-linux-android-"
-CROSS_COMPILE_ARM32+="$PWD/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-"
+rm -rf AnyKernel
+if [ ! -d clang ]
+then
+git clone --depth=1 https://github.com/crdroidmod/android_prebuilts_clang_host_linux-x86_clang-5407736 clang
 
-# Modules environtment
-OUTDIR="$PWD/out/"
-SRCDIR="$PWD/"
-MODULEDIR="$PWD/AnyKernel3/modules/system/lib/modules/"
-PRIMA="$PWD/AnyKernel3/modules/vendor/lib/modules/wlan.ko"
-PRONTO="$PWD/AnyKernel3/modules/vendor/lib/modules/pronto/pronto_wlan.ko"
-STRIP="$PWD/aarch64-linux-android-4.9/bin/$(echo "$(find "$PWD/aarch64-linux-android-4.9/bin" -type f -name "aarch64-*-gcc")" | awk -F '/' '{print $NF}' |\
-			sed -e 's/gcc/strip/')"
+fi
 
-# Export
+if [ ! -d gcc32 ]
+then
+git clone --depth=1 https://github.com/KudProject/arm-linux-androideabi-4.9 gcc32
+fi
+
+if [ ! -d gcc ]
+then
+git clone --depth=1 https://github.com/KudProject/aarch64-linux-android-4.9 gcc
+fi
+
+if [ ! -d AnyKernel ]
+then
+git clone https://gitlab.com/CyberJalagam/AnyKernel3 -b rm1 --depth=1 AnyKernel
+fi
+
+echo "Done"
+
+
+KERNEL_DIR=$(pwd)
+IMAGE="${KERNEL_DIR}/out/arch/arm64/boot/Image.gz-dtb"
+TANGGAL=$(date +"%Y%m%d-%H")
+BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+PATH="${KERNEL_DIR}/clang/bin:${KERNEL_DIR}/gcc/bin:${KERNEL_DIR}/gcc32/bin:${PATH}"
+export KBUILD_COMPILER_STRING="$(${KERNEL_DIR}/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')"
 export ARCH=arm64
-export SUBARCH=arm64
-export CROSS_COMPILE
-export CROSS_COMPILE_ARM32
-export KBUILD_BUILD_USER=radeon
-export KBUILD_BUILD_HOST=LV
+export KBUILD_BUILD_USER=jaishnav
+export KBUILD_BUILD_HOST=rbinternational
 
-# Main script
-while true; do
-	echo -e "\n[1] Build Kernel"
-	echo -e "[2] Regenerate defconfig"
-	echo -e "[3] Source cleanup"
-	echo -e "[4] Create flashable zip"
-	echo -e "[5] Quit"
-	echo -ne "\n(i) Please enter a choice[1-5]: "
+# Compile plox
+function compile() {
 
-	read choice
-
-	if [ "$choice" == "1" ]; then
-		echo -e "\n(i) Cloning toolcahins if folder not exist..."
-		git clone https://gitlab.com/Baibhab34/aarch64-linux-android-4.9.git --depth 69
-                git clone https://gitlab.com/Baibhab34/arm-linux-androideabi-4.9.git --depth 69
-		echo -e ""
-		make  O=out $CONFIG $THREAD &>/dev/null
-		make  O=out $THREAD & pid=$!
-
-		BUILD_START=$(date +"%s")
-		DATE=`date`
-
-		echo -e "\n#######################################################################"
-
-		echo -e "(i) Build started at $DATE using $CORES thread"
-
-		spin[0]="-"
-		spin[1]="\\"
-		spin[2]="|"
-		spin[3]="/"
-		echo -ne "\n[Please wait...] ${spin[0]}"
-		while kill -0 $pid &>/dev/null
-		do
-			for i in "${spin[@]}"
-			do
-				echo -ne "\b$i"
-				sleep 0.1
-			done
-		done
-
-		if ! [ -a $KERN_IMG ]; then
-			echo -e "\n(!) Kernel compilation failed, See buildlog to fix errors"
-			echo -e "#######################################################################"
-		fi
-
-		BUILD_END=$(date +"%s")
-		DIFF=$(($BUILD_END - $BUILD_START))
-
-		echo -e "\nImage-dtb compiled successfully."
-
-		echo -e "#######################################################################"
-
-		echo -e "(i) Total time elapsed: $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
-
-		echo -e "#######################################################################"
+    echo -e "${CYAN}"
+    make -j$(nproc) O=out ARCH=arm64 oppo6771_17065_defconfig
+    make -j$(nproc) O=out \
+                    ARCH=arm64 \
+                    CC=clang \
+                    CLANG_TRIPLE=aarch64-linux-gnu- \
+                    CROSS_COMPILE=aarch64-linux-android- \
+                    CROSS_COMPILE_ARM32=arm-linux-androideabi-
+   echo -e "${RST}"
+SUCCESS=$?
+	if [ $SUCCESS -eq 0 ]
+        	then
+		echo -e "${GRN}"
+		echo "------------------------------------------------------------"
+		echo "Compilation successful..."
+        	echo "Image.gz-dt can be found at out/arch/arm64/boot/Image.gz-dtb"
+    		cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
+		echo  "------------------------------------------------------------"
+		echo -e "${RST}"
+	else
+		echo -e "${RED}"
+                echo "------------------------------------------------------------"
+		echo "Compilation failed..check build logs for errors"
+                echo "------------------------------------------------------------"
+		echo -e "${RST}"
 	fi
 
-	if [ "$choice" == "2" ]; then
-		echo -e "\n#######################################################################"
+}
+# Zipping
+function zipping() {
+    echo -e "${YELLOW}"
+    echo "Creating a flashable zip....."
+    cd AnyKernel || exit 1
+    zip -r9 AlienKernel™️-v1.0-RMX1831-${TANGGAL}.zip * > /dev/null 2>&1
+    cd ..
+    echo "Zip stored at AnyKernel/AlienKernel™️-v1.0-RMX1831-${TANGGAL}.zip"
+    echo -e "${RST}"
+}
+compile
 
-		make O=out  $CONFIG savedefconfig &>/dev/null
-		cp out/defconfig arch/arm64/configs/$CONFIG &>/dev/null
-
-		echo -e "(i) Defconfig generated."
-
-		echo -e "#######################################################################"
-	fi
-
-	if [ "$choice" == "3" ]; then
-		echo -e "\n#######################################################################"
-
-		make O=out clean &>/dev/null
-		make mrproper &>/dev/null
-		rm -rf out/*
-
-		echo -e "(i) Kernel source cleaned up."
-
-		echo -e "#######################################################################"
-	fi
-
-	if [ "$choice" == "4" ]; then
-		echo -e "\n#######################################################################"
-        echo -e "\n(i) Cloning AnyKernel3 if folder not exist..."
-		git clone -b stormbreaker https://github.com/RADEON7/AnyKernel3.git
-		echo -e "\n(i) Strip and move modules to AnyKernel3..."
-
-		# thanks to @adekmaulana
-
-		cd $ZIP_DIR
-		make clean &>/dev/null
-		cd ..
-
-		for MOD in $(find "${OUTDIR}" -name '*.ko') ; do
-			"${STRIP}" --strip-unneeded --strip-debug "${MOD}" &> /dev/null
-			"${SRCDIR}"/scripts/sign-file sha512 \
-					"${OUTDIR}/signing_key.priv" \
-					"${OUTDIR}/signing_key.x509" \
-					"${MOD}"
-			find "${OUTDIR}" -name '*.ko' -exec cp {} "${MODULEDIR}" \;
-			case ${MOD} in
-				*/wlan.ko)
-					cp -ar "${MOD}" "${PRIMA}"
-					cp -ar "${MOD}" "${PRONTO}"
-			esac
-		done
-		echo -e "\n(i) Done moving modules"
-
-		rm $PWD/AnyKernel3/modules/system/lib/modules/wlan.ko
-		cd $ZIP_DIR
-		cp $KERN_IMG $ZIP_DIR/zImage
-		make normal &>/dev/null
-		cd ..
-
-		echo -e "Flashable zip generated under $ZIP_DIR."
-
-		echo -e "#######################################################################"
-	fi
-
-	if [ "$choice" == "5" ]; then
-		exit
-	fi
-
-done
-echo -e "$nc"
-
+if [ $SUCCESS -eq 0 ]
+then
+	zipping
+fi
